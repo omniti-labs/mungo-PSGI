10cbdea4909a3f619511e1bf26997705d3184357
Graham Knop <gknop@omniti.com>
2012-03-12 15:27:12 -0400

    test module provides base file dir

--- t/lib/Mungo/Test.pm
+++ t/lib/Mungo/Test.pm
@@ -6,13 +6,24 @@ use Try::Tiny;
 use Test::MockObject;
 use Mungo::PSGI::Request;
 use Mungo::PSGI::Script::Memory;
+use File::Spec::Functions qw(updir catdir);
+use File::Basename qw(dirname);
+use Cwd qw(abs_path);
 
 our @EXPORT = qw(run_asp asp_like);
 my $CLASS = __PACKAGE__;
 
+my $data = abs_path(catdir(dirname(__FILE__), updir, updir, 'data'));
+
+sub data {
+    $data;
+}
+
 sub run_asp {
     my $asp = shift;
-    my $req = Mungo::PSGI::Request->new({});
+    my $req = Mungo::PSGI::Request->new({
+        'mungo.file_base' => $data,
+    });
     $req->Response->body([]);
     my $script = Mungo::PSGI::Script::Memory->new($asp, @_);
     $script->run($req);
