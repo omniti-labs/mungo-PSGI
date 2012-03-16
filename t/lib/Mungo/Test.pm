03372fa1a26e901ba19c4cedf5922485eb49fd41
Graham Knop <gknop@omniti.com>
2012-03-16 05:17:21 -0400

    allow testing more than just output text

--- t/lib/Mungo/Test.pm
+++ t/lib/Mungo/Test.pm
@@ -8,6 +8,7 @@ use Mungo::PSGI::Script::Memory;
 use File::Spec::Functions qw(updir catdir);
 use File::Basename qw(dirname);
 use Cwd qw(abs_path);
+use Data::Dumper ();
 
 our @EXPORT = qw(run_asp asp_like);
 my $CLASS = __PACKAGE__;
@@ -25,26 +26,76 @@ sub run_asp {
     });
     $req->Response->body([]);
     my $script = Mungo::PSGI::Script::Memory->new($asp, @_);
-    $script->run($req);
+    try {
+        $script->run($req);
+    }
+    catch {
+        local $SIG{__DIE__};
+        unless ($_ && ref $_ && ref $_ eq 'ARRAY' && $_->[0] eq 'Mungo::End') {
+            die $_;
+        }
+    };
     my $output = join '', @{ $req->Response->body };
-    return $output;
+    return wantarray ? ($output, $req->Response) : $output;
+}
+
+sub _dump {
+    local $Data::Dumper::Terse = 1;
+    local $Data::Dumper::Useqq = 1;
+    my $plain = Data::Dumper::Dumper(shift);
+    chomp $plain;
+    return $plain;
 }
 
 sub asp_like {
     my $asp = shift;
     my $like = shift;
     my $Test = $CLASS->builder;
-    my @params = @_;
+    my $name = shift;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
     try {
         local $Test::Builder::Level = $Test::Builder::Level + 1;
-        my $output = run_asp($asp, @params);
-        $Test->like($output, qr/$like/x, @params);
+        my ($output, $resp) = run_asp($asp, $name);
+        if (ref $like) {
+            $like->{status}
+                and $Test->is_num($resp->code, $like->{status}, "$name status");
+            if ($like->{headers}) {
+                my $nok = '';
+                my @headers = @{ $like->{headers} };
+                while (@headers) {
+                    my ($name, $value) = (shift @headers, shift @headers);
+                    my $got = _dump($resp->header($name));
+                    $value = _dump($value);
+                    if ($got ne $value) {
+                        $nok .= "expected $value, got $got for header $name\n";
+                    }
+                }
+                $Test->ok( ! $nok, "$name headers")
+                    or $Test->diag($nok);
+            }
+            $like = $like->{output};
+        }
+        if ($like) {
+            $Test->like($output, qr/$like/x, "$name output");
+        }
     }
     catch {
         local $Test::Builder::Level = $Test::Builder::Level + 1;
-        $Test->ok(0, @params);
-        $Test->diag($_);
+        my $err = $_;
+        if (ref $like) {
+            if ($like->{error}) {
+                $Test->like($err, qr/$like->{error}/x, "$name error");
+            }
+            else {
+                $Test->ok(0, "$name $_")
+                    for sort keys %$like;
+                $Test->diag($err);
+            }
+        }
+        else {
+            $Test->ok(0, "$name output");
+            $Test->diag($err);
+        }
     };
 }
 
